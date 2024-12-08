4270026e7c34c6baa4b7b674e857069d81c4a872
lf888nmdd <2052163735@qq.com>
2024-12-08 19:50:26 +0800

    添加header文件

--- film-web/src/pages/_app.tsx
+++ film-web/src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import { AppProps } from 'next/app';
+import Header from'@/Component/Header.tsx'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
+      <Header />
       <Component {...pageProps} />
     </div>
   );
