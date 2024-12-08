24309659ef5bb4d5cf9aa71cafed518d0a18827d
lf888nmdd <2052163735@qq.com>
2024-12-08 17:39:18 +0800

    电影官网web端基本框架

---
+++ film-web/next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  trailingSlash: true
+};
+
+export default nextConfig;
